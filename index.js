a6d849534465f2d43e0bbd7947e9183acffd52d6
Yonela Johannes <johannesyonela@gmail.com>
2023-10-11 15:41:03 +0200

    Chore: production url cors

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(passport.initialize());
 app.use(passport.session())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }))
-app.use(cors({origin: ['http://localhost:3000', 'https://yonela-johannes.github.io/mysite/', 'https://johannesyonela.engineer'], credentials: true}));
+app.use(cors({origin: ['http://localhost:3000', 'https://yonela-johannes.github.io', 'https://yonela-johannes.github.io/mysite', 'https://johannesyonela.engineer'], credentials: true}));
 
 app.use("/api/user", userRouter);
 app.use("/api/reviews", reviewRouter);
